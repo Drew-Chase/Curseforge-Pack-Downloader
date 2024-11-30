5582ed69e129166ab657fb8113d63fdd4ec8a939
Drew Chase (Home) <dcmanproductions@gmail.com>
2024-11-29 20:00:01 -0500

    Add command line argument parsing with Clap
    Introduce a new module `commandline_args.rs` to handle command line arguments using the Clap library. This implementation supports parsing options for project ID, input file, output path, and a server-only mode. The setup ensures mutually exclusive constraints between the ID and file arguments.

---
+++ src/commandline_args.rs
@@ -0,0 +1,20 @@
+use clap::Parser;
+
+#[derive(Parser, Debug, Clone)]
+pub struct CommandlineArgs {
+    /// The Project ID
+    #[arg(short, long, conflicts_with = "file", required_unless_present = "file")]
+    pub id: Option<u64>,
+
+    /// The input pack archive file
+    #[arg(short, long, conflicts_with = "id", required_unless_present = "id")]
+    pub file: Option<String>,
+
+    /// This is the output zip path
+    #[arg(short, long, default_value = "output")]
+    pub output: Option<String>,
+
+    /// This will only download serverside mods
+    #[arg(long)]
+    pub server_only: bool,
+}
