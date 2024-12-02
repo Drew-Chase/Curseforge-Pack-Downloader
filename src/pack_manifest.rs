f840f18a965084d84ea60f40a0ee6751d7d5b16e
Drew Chase (Home) <dcmanproductions@gmail.com>
2024-12-02 18:09:49 -0500

    Log directory path in download_mods method
    Add a print statement to log the directory path where mods are being downloaded in the download_mods function. This helps in debugging by providing visibility of the directory input during the download process.

--- src/pack_manifest.rs
+++ src/pack_manifest.rs
@@ -46,6 +46,7 @@ impl Manifest {
     }
 
     pub async fn download_mods(&self, directory: impl AsRef<Path>, parallel: u8, validate:bool, validate_if_size_less_than: Option<u64>) -> Result<(), Box<dyn Error>> {
+        println!("-> {:?}", directory.as_ref());
         crate::curseforge_api::download_mods_from_manifest(self, directory, parallel,validate, validate_if_size_less_than).await
     }
 }
