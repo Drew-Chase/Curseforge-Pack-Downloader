7e275b04c4142c430e9b2b38076cb3702f49b064
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-12-02 14:18:31 -0500

    Add validation and parallel downloads to CLI args
    Enhance command line functionality by adding options for mod validation and parallel download support. The `validate` flag allows users to verify downloaded mods through a hash, and `parallel_downloads` enables simultaneous downloads to speed up the process. Additionally, the `validate_if_size_less_than` option optimizes validation for smaller files only.

--- src/commandline_args.rs
+++ src/commandline_args.rs
@@ -11,10 +11,20 @@ pub struct CommandlineArgs {
     pub file: Option<String>,
 
     /// This is the output zip path
-    #[arg(short, long, default_value = "output")]
+    #[arg(short, long, default_value = "./")]
     pub output: Option<String>,
 
-//     This will only download serverside mods
-//    #[arg(long)]
-//    pub server_only: bool,
+    /// This will validate the downloaded mods based on the provided hash. (Note: this can take significantly longer)
+    #[arg(long)]
+    pub validate: bool,
+
+    /// The number of mods that will be downloaded in parallel
+    /// This can allow for much faster downloading.
+    /// Higher may not be better, due to internet speeds.
+    #[arg(short, long, default_value_t = 16, value_name="NUMBER")]
+    pub parallel_downloads: u8,
+
+    /// This will only attempt to validate files where the file size is less than this value (in bytes)
+    #[arg(long, requires = "validate", value_name = "BYTES")]
+    pub validate_if_size_less_than: Option<u64>,
 }
