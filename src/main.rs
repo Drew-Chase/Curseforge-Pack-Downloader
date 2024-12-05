a8bc752402094cd5a72e92e784b9eb6197e7bc5e
Drew Chase (Home) <dcmanproductions@gmail.com>
2024-12-05 12:31:14 -0500

    Refactor downloader logic in main.rs
    Streamlined the main.rs file by integrating the `CurseforgePackDownloader` and removing redundant modules and logic related to pack handling. Improved the error handling and logging setup and ensured the process flow is clearer and more concise, focusing on parsing and processing command-line arguments and environmental variables efficiently.

--- src/main.rs
+++ src/main.rs
@@ -5,109 +5,75 @@
 
 use crate::commandline_args::CommandlineArgs;
 use clap::Parser;
+use curseforge_pack_downloader::CurseforgePackDownloader;
 use log::{error, info, warn};
 use std::env::set_var;
-use std::path::PathBuf;
 use std::process::exit;
 
 mod commandline_args;
-mod curseforge_api;
 mod env;
-mod mod_file;
-mod mod_type;
-mod pack_archive;
-mod pack_manifest;
-mod project_structure;
 
 #[tokio::main]
 async fn main() {
+    // Record the start time of the process
     let start_time = std::time::SystemTime::now();
+
+    // Parse command line arguments
     let args = CommandlineArgs::parse();
 
+    // Set up the logging environment
     set_var("RUST_LOG", "info");
     env_logger::init();
+
+    // Log starting information about the tool
     info!("Starting unfuck-curseforge");
     warn!("This tool is not affiliated with CurseForge in any way, in fact we strongly dislike curseforge's bullshit!");
 
-    match env::Env::new() {
-        Ok(env) => set_var("CURSEFORGE_API_KEY", env.curseforge_api_key),
+    // Attempt to read environment variables from the embedded env.ini file
+    let env = match env::Env::new() {
+        Ok(env) => env,
         Err(err) => {
+            // Log an error and exit if env variables cannot be read
             error!("Unable to read environment variables from the env.ini file, make sure its filled out before building: {}", err);
             exit(1);
         }
-    }
-    
-    println!(
-        "Using CurseForge API Key: {}",
-        std::env::var("CURSEFORGE_API_KEY").unwrap_or("NOT SET".to_string()));
+    };
 
-    if args.id.is_none() && args.file.is_none() {
+    // set the `CURSEFORGE_API_KEY` environment variable
+    // this will be required by the `CurseforgePackDownloader`
+    set_var("CURSEFORGE_API_KEY", &env.curseforge_api_key);
+
+    // Create an instance of the `CurseforgePackDownloader` struct.
+    let mut downloader = CurseforgePackDownloader::new();
+
+    // Set downloader options based on input arguments
+    downloader.set_validate(args.validate);
+    downloader.set_parallel_downloads(args.parallel_downloads);
+
+    // Set validation size limit if provided and validation is enabled
+    if let Some(validate_if_less_than_bytes) = args.validate_if_size_less_than {
+        downloader.set_validate_if_size_less_than(validate_if_less_than_bytes);
+    }
+
+    // Determine processing path based on input ID or file
+    match if let Some(id) = args.id {
+        downloader.process_id(id).await
+    } else if let Some(file) = args.file {
+        downloader.process_file(file).await
+    } else {
+        // Log an error if neither an ID nor a file is specified and exit
         error!("You must specify a url or file to download");
         exit(1);
-    }
-    let mut file: Option<PathBuf> = None;
-
-    if let Some(project_id) = args.id {
-        file = Some(
-            match curseforge_api::download_latest_pack_archive(project_id).await {
-                Ok(file) => file,
-                Err(err) => {
-                    error!("Unable to download the latest pack archive: {}", err);
-                    exit(1);
-                }
-            },
-        );
-    }
-
-    if let Some(file_string) = args.file {
-        file = Some(PathBuf::from(file_string));
-    }
-    if let Some(file) = file {
-        let (tmp, manifest) = match pack_archive::process_archive(
-            file,
-            args.parallel_downloads,
-            args.validate,
-            args.validate_if_size_less_than,
-        )
-        .await
-        {
-            Ok(output) => output,
-            Err(err) => {
-                error!("Unable to process the pack archive: {}", err);
-                exit(1);
-            }
-        };
-
-        let mods = tmp.join("mods");
-        let overrides = tmp.join("overrides");
-
-        if let Some(output_path) = args.output {
-            let output_path = output_path.join(format!(
-                "{}-{}",
-                manifest.name,
-                match std::time::SystemTime::now().duration_since(std::time::SystemTime::UNIX_EPOCH)
-                {
-                    Ok(duration) => duration.as_millis(),
-                    Err(err) => {
-                        error!("Unable to get current time: {}", err);
-                        0
-                    }
-                }
-            ));
-            match pack_archive::copy_to_output(mods, overrides, output_path) {
-                Ok(output) => {
-                    info!("Pack copied to {}", output.display());
-                }
-                Err(err) => {
-                    error!("Unable to copy pack to output: {}", err);
-                }
-            };
+    } {
+        Ok(manifest) => manifest,
+        Err(err) => {
+            // Log an error and exit if processing fails
+            error!("Failed to process pack: {}", err);
+            exit(1);
         }
-    } else {
-        error!("File not provided!");
-        exit(1);
-    }
+    };
 
+    // Calculate and log the duration of the process
     let end_time = std::time::SystemTime::now();
     let duration = end_time.duration_since(start_time).unwrap_or_else(|err| {
         error!("Unable to get current time: {}", err);
