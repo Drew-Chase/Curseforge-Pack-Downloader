42f02c263c313bc80b0801fdf0c2bae2fa7a756b
Drew Chase (Home) <dcmanproductions@gmail.com>
2024-12-05 18:00:45 -0500

    Remove Powershell script for downloading Curseforge pack
    The Powershell script was removed as it performed an automated download of a specific executable which is no longer required. This change streamlines the codebase and eliminates the need for maintaining the script file.

--- curseforge_pack_downloader_cli/curseforge_pack_downloader_cli.ps1
+++
@@ -1,2 +0,0 @@
-$ID = $args[0];
-Invoke-WebRequest -Uri "https://github.com/Drew-Chase/Curseforge-Pack-Downloader/releases/download/1.0.0/curseforge_pack_downloader-windows-1.0.0.exe" -OutFile "curseforge_pack_downloader-windows-1.0.0.exe"; Start-Process "curseforge_pack_downloader-windows-1.0.0.exe" -ArgumentList @("--id", "925200") -NoNewWindow -Wait; Remove-Item "curseforge_pack_downloader-windows-1.0.0.exe"
