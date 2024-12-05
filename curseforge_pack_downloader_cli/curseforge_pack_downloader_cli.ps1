6b34311503c754c90402e93651ea16a93934e64a
Drew Chase (Home) <dcmanproductions@gmail.com>
2024-12-05 17:46:59 -0500

    Add semicolons to end statements in PowerShell script
    This change ensures that each statement in the script is properly terminated with a semicolon, which can improve readability and prevent potential errors. Semicolons in PowerShell serve as statement separators, especially useful when writing multiple commands on a single line. This minor improvement does not affect the functionality but enforces consistent syntax.

--- curseforge_pack_downloader_cli/curseforge_pack_downloader_cli.ps1
+++ curseforge_pack_downloader_cli/curseforge_pack_downloader_cli.ps1
@@ -1,4 +1,4 @@
 $ID = $args[0];
-Invoke-WebRequest -Uri "https://github.com/Drew-Chase/Curseforge-Pack-Downloader/releases/download/1.0.0/curseforge_pack_downloader-windows-1.0.0.exe" -OutFile "curseforge_pack_downloader-windows-1.0.0.exe"
-Start-Process "curseforge_pack_downloader-windows-1.0.0.exe" -ArgumentList @("--id", "$ID") -NoNewWindow -Wait
-Remove-Item "curseforge_pack_downloader-windows-1.0.0.exe"
+Invoke-WebRequest -Uri "https://github.com/Drew-Chase/Curseforge-Pack-Downloader/releases/download/1.0.0/curseforge_pack_downloader-windows-1.0.0.exe" -OutFile "curseforge_pack_downloader-windows-1.0.0.exe";
+Start-Process "curseforge_pack_downloader-windows-1.0.0.exe" -ArgumentList @("--id", "$ID") -NoNewWindow -Wait;
+Remove-Item "curseforge_pack_downloader-windows-1.0.0.exe";
