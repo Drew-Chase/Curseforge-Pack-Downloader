ea496d92a5aef9384713456c92510de898530472
Drew Chase (Home) <dcmanproductions@gmail.com>
2024-12-05 23:50:41 -0500

    Add Env struct to manage environment variables
    Introduces an Env struct to load and store configuration from an embedded env.ini file. This implementation reads key-value pairs, filters comments, and extracts the CurseForge API key for application use. This setup improves configuration management by centralizing environment variable handling within the app.

---
+++ curseforge-pack-downloader-app/src-tauri/src/env.rs
@@ -0,0 +1,49 @@
+use log::info;
+use std::collections::HashMap;
+use std::error::Error;
+
+static ENV_FILE: &str = include_str!("../../../env.ini");
+
+#[derive(Debug, Default)]
+pub struct Env {
+    pub curseforge_api_key: String,
+}
+
+impl Env {
+    pub fn new() -> Result<Env, Box<dyn Error>> {
+        info!("Reading environment variables from embedded file");
+        // get an array of lines
+        let lines = ENV_FILE.lines();
+
+        // remove any lines that start with '#'
+        let lines = lines.filter(|line| !line.starts_with('#'));
+
+        // remove any data after '#'
+        // Ex: key=value #some other data
+        // the #some other data will be trimmed
+        let lines: Vec<&str> = lines
+            .map(|line| line.split('#').next().unwrap_or(line))
+            .collect();
+
+        // get key-value pairs
+        let kvp: HashMap<String, String> = lines
+            .iter()
+            .map(|line| {
+                let mut split = line.split("=");
+                let key = split.next().unwrap_or("").to_string();
+                let value = split.next().unwrap_or("").to_string();
+                (key, value)
+            })
+            .collect();
+
+        let mut env: Env = Env::default();
+
+        for (key, value) in kvp {
+            if key.eq_ignore_ascii_case("curseforge_api_key") {
+                env.curseforge_api_key = value;
+            }
+        }
+
+        Ok(env)
+    }
+}
