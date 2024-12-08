6d22f5e16120d3ce2ef3a7ffcea9f8eef03120f5
Drew Chase (Home) <dcmanproductions@gmail.com>
2024-12-08 17:34:21 -0500

    Add version selection for pack downloads
    Introduce a modal to select specific modpack versions before download in PackItemComponent. Also refactor to include version fetching and update the unpacking process to use the selected file ID. This enhancement provides users greater flexibility in downloading specific versions or the latest by default.

--- curseforge-pack-downloader-app/src/assets/components/PackItemComponent.tsx
+++ curseforge-pack-downloader-app/src/assets/components/PackItemComponent.tsx
@@ -1,8 +1,11 @@
-import {Avatar, Button, Chip, Link} from "@nextui-org/react";
+import {Avatar, Button, ButtonGroup, Chip, Link} from "@nextui-org/react";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faDownload} from "@fortawesome/free-solid-svg-icons";
 import {open} from "@tauri-apps/plugin-shell";
 import {open as openFile} from "@tauri-apps/plugin-dialog";
+import PackVersionSelectModal from "./PackVersionSelectModal.tsx";
+import {useState} from "react";
+import get_pack_versions from "../ts/modpack_version_file.ts";
 
 interface PackItemProps
 {
@@ -13,67 +16,100 @@ interface PackItemProps
     iconUrl: string;
     gameVersion: string;
     websiteUrl: string;
-    onUnpack: (path:string) => void;
+    onUnpack: (path: string, fileId:number) => void;
 }
 
 
 export default function PackItemComponent(props: PackItemProps)
 {
+    const [selectedPackId, setSelectedPackId] = useState<number | null>(null);
     return (
-        <div className={"flex flex-col shrink-0 p-4 justify-between dark:bg-[#101010] bg-black/5 hover:dark:bg-black/70 hover:bg-black/10 rounded-lg transition-colors"}>
-            <div className={"flex flex-row shrink-0"}>
-                <Avatar src={props.iconUrl} radius={"sm"} size={"lg"} className={"mr-4"}/>
-                <div className={"flex flex-col items-start"}>
-                    <div className={"flex flex-row items-center"}>
-                        <p className={"text-large font-bold mr-2"}>{props.name}</p>
-                        <p className={"text-tiny opacity-70"}>By {props.author}</p>
+        <>
+            <PackVersionSelectModal
+                onClose={() => setSelectedPackId(null)}
+                onConfirm={async fileId =>
+                {
+                    const path = await openFile({
+                        title: "Select a location to save the pack",
+                        defaultPath: `${props.name}-${props.author}-${props.id}-${new Date().getTime()}`,
+                        directory: true,
+                        multiple: false,
+                        canCreateDirectories: true
+                    });
+                    if (path)
+                    {
+                        props.onUnpack(path, fileId);
+                    }
+                }}
+                packId={selectedPackId}
+            />
+            <div className={"flex flex-col shrink-0 p-4 justify-between dark:bg-[#101010] bg-black/5 hover:dark:bg-black/70 hover:bg-black/10 rounded-lg transition-colors"}>
+                <div className={"flex flex-row shrink-0"}>
+                    <Avatar src={props.iconUrl} radius={"sm"} size={"lg"} className={"mr-4"}/>
+                    <div className={"flex flex-col items-start"}>
+                        <div className={"flex flex-row items-center"}>
+                            <p className={"text-large font-bold mr-2"}>{props.name}</p>
+                            <p className={"text-tiny opacity-70"}>By {props.author}</p>
+                        </div>
+                        <p className={"opacity-70"}>{props.description}</p>
                     </div>
-                    <p className={"opacity-70"}>{props.description}</p>
                 </div>
-            </div>
-            <div className={"flex flex-row mt-4 gap-3 items-center"}>
-                <div className={"flex flex-row gap-3 mr-auto"}>
-                    <Chip radius={"sm"} color={"default"} variant={"flat"}>
+                <div className={"flex flex-row mt-4 gap-3 items-center"}>
+                    <div className={"flex flex-row gap-3 mr-auto"}>
+                        <Chip radius={"sm"} color={"default"} variant={"flat"}>
                         <span className={"flex flex-row gap-2 items-center"}>
                             {props.gameVersion}
                         </span>
-                    </Chip>
+                        </Chip>
+                    </div>
+                    <Button
+                        as={Link}
+                        variant={"flat"}
+                        showAnchorIcon
+                        isExternal
+                        onClick={
+                            async () =>
+                            {
+                                await open(props.websiteUrl);
+                            }
+                        }
+                    >
+                        View on Curseforge
+                    </Button>
+                    <ButtonGroup>
+
+                        <Button
+                            color={"primary"}
+                            startContent={<FontAwesomeIcon icon={faDownload} className={"mr-1"}/>}
+                            onClick={async () =>
+                            {
+                                const path = await openFile({
+                                    title: "Select a location to save the pack",
+                                    defaultPath: `${props.name}-${props.author}-${props.id}-${new Date().getTime()}`,
+                                    directory: true,
+                                    multiple: false,
+                                    canCreateDirectories: true
+                                });
+                                if (path)
+                                {
+                                    const files = await get_pack_versions(props.id)
+                                    const latest = files[0].id;
+                                    props.onUnpack(path, latest);
+                                }
+                            }}
+                        >
+                            Download Latest
+                        </Button>
+                        <Button
+                            variant={"ghost"}
+                            color={"primary"}
+                            onClick={() => setSelectedPackId(props.id)}
+                        >
+                            Select Version
+                        </Button>
+                    </ButtonGroup>
                 </div>
-                <Button
-                    as={Link}
-                    variant={"flat"}
-                    showAnchorIcon
-                    isExternal
-                    onClick={
-                        async () =>
-                        {
-                            await open(props.websiteUrl);
-                        }
-                    }
-                >
-                    View on Curseforge
-                </Button>
-                <Button
-                    color={"primary"}
-                    startContent={<FontAwesomeIcon icon={faDownload} className={"mr-1"}/>}
-                    onClick={async () =>
-                    {
-                        const path = await openFile({
-                            title: "Select a location to save the pack",
-                            defaultPath: `${props.name}-${props.author}-${props.id}-${new Date().getTime()}`,
-                            directory: true,
-                            multiple: false,
-                            canCreateDirectories: true
-                        });
-                        if (path)
-                        {
-                            props.onUnpack(path);
-                        }
-                    }}
-                >
-                    Download
-                </Button>
             </div>
-        </div>
+        </>
     );
 }
